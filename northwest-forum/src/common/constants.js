a01787153864da21de08627078cdb8b55bad9e2d
samuilYoshkov <magmatogorsk@protonmail.com>
2023-09-16 15:19:50 +0300

    add constants

---
+++ northwest-forum/src/common/constants.js
@@ -0,0 +1,20 @@
+export const categories = [
+'Тренировъчни програми за повишаване на теглото',
+'Тренировъчни програми за отслабване',
+'Упражнения',
+'Проблемни мускулни групи',
+'Травми и здравословни проблеми',
+'Outdoor Workout',
+'Хранителни режими за покачване на тегло',
+'Хранителни режими за отслабване',
+'Хранителни добавки за сила и издръжливост',
+'Храни и хранителни продукти',
+'Травми и контузии',
+'Общи теми за здраве',
+'Спортни аксесоари',
+'Фитнес уреди',
+'Фитнес центрове',
+];
+
+
+export const projectInfo = "Welcome to our Northwest Fitness forum! Here, we provide a community-driven space for people of all fitness levels to come together and discuss everything related to health and wellness. Whether you're a seasoned athlete or just starting your fitness journey, this forum is the perfect place to connect with others who share your interests and goals. From workout routines to nutrition tips, injury prevention to mental health strategies, our forum covers a wide range of topics to help you reach your fullest potential. So come join the conversation and let's support each other on our path to a healthier, happier life!"

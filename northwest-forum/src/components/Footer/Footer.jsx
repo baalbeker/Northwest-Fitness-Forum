45eb52bacbb994472bf10a3c8bd673afbcc83d07
samuilYoshkov <magmatogorsk@protonmail.com>
2023-09-16 15:12:39 +0300

    add footer

---
+++ northwest-forum/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>&copy; Northwest Fitness FORUM {new Date().getFullYear()}</p>
+        </div>
+    )
+}
+
+export default Footer;
